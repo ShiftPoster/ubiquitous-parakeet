0600f2eb97f38c47f5a132124d85368c905817c9
Shift Poster <86fullers_secrets@icloud.com>
2026-05-16 23:48:46 -0400

    moved solution to leetcode module

---
+++ src/leetcode/p2_add.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: ListNode | None, l2: ListNode | None
+    ) -> ListNode | None:
+        if l1 is None and l2 is None:
+            start = None
+        else:
+            carry = False
+            start = ListNode()
+            sum_ll = start
+            curr_1 = ListNode() if l1 is None else l1
+            curr_2 = ListNode() if l2 is None else l2
+
+            while True:
+                sum_ll.val = curr_1.val
+                sum_ll.val += curr_2.val
+                sum_ll.val += 1 if carry else 0
+                carry = sum_ll.val >= 10
+                sum_ll.val -= 10 if carry else 0
+
+                if curr_1.next is None and curr_2.next is None:
+                    break
+
+                curr_1 = ListNode() if curr_1.next is None else curr_1.next
+                curr_2 = ListNode() if curr_2.next is None else curr_2.next
+
+                new = ListNode()
+                sum_ll.next = new
+                sum_ll = new
+
+            if carry is True:
+                sum_ll.next = ListNode(1)
+
+        return start
+
+
+def run(l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
+    return Solution().addTwoNumbers(l1, l2)
--- tests/test_add_two_numbers.py
+++ tests/test_add_two_numbers.py
@@ -11,6 +11,8 @@ from itertools import zip_longest
 
 import pytest
 
+from leetcode.p2_add import ListNode, run
+
 
 class Case(Enum):
     example1 = [2, 4, 3], [5, 6, 4], [7, 0, 8]
@@ -23,7 +25,7 @@ class Case(Enum):
         self.sol = sol
 
 
-def pseudo(l1, l2):
+def validate(l1, l2):
     solution = []
     carry = False
     for i, j in zip_longest(l1, l2, fillvalue=0):
@@ -41,51 +43,8 @@ def pseudo(l1, l2):
 
 
 @pytest.mark.parametrize("case", Case)
-def test_pseudo(case: Case):
-    assert pseudo(case.l1, case.l2) == case.sol
-
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-class Solution:
-    def addTwoNumbers(
-        self, l1: ListNode | None, l2: ListNode | None
-    ) -> ListNode | None:
-        if l1 is None and l2 is None:
-            start = None
-        else:
-            carry = False
-            start = ListNode()
-            sum_ll = start
-            curr_1 = ListNode() if l1 is None else l1
-            curr_2 = ListNode() if l2 is None else l2
-
-            while True:
-                sum_ll.val = curr_1.val
-                sum_ll.val += curr_2.val
-                sum_ll.val += 1 if carry else 0
-                carry = sum_ll.val >= 10
-                sum_ll.val -= 10 if carry else 0
-
-                if curr_1.next is None and curr_2.next is None:
-                    break
-
-                curr_1 = ListNode() if curr_1.next is None else curr_1.next
-                curr_2 = ListNode() if curr_2.next is None else curr_2.next
-
-                new = ListNode()
-                sum_ll.next = new
-                sum_ll = new
-
-            if carry is True:
-                sum_ll.next = ListNode(1)
-
-        return start
+def test_validate(case: Case):
+    assert validate(case.l1, case.l2) == case.sol
 
 
 def to_linked(ll) -> ListNode:
@@ -110,7 +69,7 @@ def from_linked(ll: ListNode) -> list[int]:
 @pytest.mark.parametrize("case", Case)
 def test_solution(case: Case):
     lll = list(map(to_linked, (case.l1, case.l2)))
-    ans = Solution().addTwoNumbers(lll[0], lll[1])
+    ans = run(lll[0], lll[1])
     assert ans
     assert from_linked(ans) == case.sol
 
@@ -128,7 +87,7 @@ class NoneCase(Enum):
 
 @pytest.mark.parametrize("case", NoneCase)
 def test_none(case: Case):
-    ans = Solution().addTwoNumbers(
+    ans = run(
         None if case.l1 is None else to_linked(case.l1),
         None if case.l2 is None else to_linked(case.l2),
     )
